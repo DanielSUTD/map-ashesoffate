aeb2b3c197864fe4630e6ac8fafe2fb2e400c64c
Carla <carlaljovito@gmail.com>
2025-05-25 20:33:40 -0300

    finalizando o mapa

---
+++ MapImages/tsx/bones.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bones" tilewidth="16" tileheight="16" tilecount="3" columns="3">
+ <image source="../bones.png" width="53" height="31"/>
+</tileset>
